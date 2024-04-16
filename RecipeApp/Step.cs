f13d3ccca45a5a6e59fb15e722fd60035278ad15
NevilleKabamba <129751496+NevilleKabamba@users.noreply.github.com>
2024-04-16 02:43:04 +0200

    Update Step.cs

--- RecipeApp/Step.cs
+++ RecipeApp/Step.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 
 namespace RecipeApp
 {
-    // Class to represent a step in a recipe
     public class Step
     {
         public string Description { get; set; } // Description of the step
