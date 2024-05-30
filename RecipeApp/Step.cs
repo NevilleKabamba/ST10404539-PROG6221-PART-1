50b547537b061eaad47ddf7ce286c9a14fade311
NevilleKabamba <st10226298@vcconnect.edu.za>
2024-05-30 02:40:07 +0200

    Update

--- RecipeApp/Step.cs
+++ RecipeApp/Step.cs
@@ -2,7 +2,7 @@
 {
     public class Step
     {
-        public string Description { get; set; }
+        public string Description { get; set; } // Description of the step
 
         public Step(string description)
         {
