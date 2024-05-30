9a7a2d469ebaf8236258880bc1ffc140d3349b33
NevilleKabamba <st10226298@vcconnect.edu.za>
2024-05-30 02:18:00 +0200

    Ingredient Update

--- RecipeApp/Ingredient.cs
+++ RecipeApp/Ingredient.cs
@@ -1,16 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RecipeApp
+﻿namespace RecipeApp
 {
     public class Ingredient
     {
-        public string Name { get; set; } // Name of the ingredients
-        public double Quantity { get; set; } // Quantity of the ingredient
-        public string Unit { get; set; } // Unit of measurement for the ingredient quantity
+        public string Name { get; set; }
+        public double Quantity { get; set; }
+        public string Unit { get; set; }
+        public int Calories { get; set; }
+        public string FoodGroup { get; set; }
+
+        public Ingredient(string name, double quantity, string unit, int calories, string foodGroup)
+        {
+            Name = name;
+            Quantity = quantity;
+            Unit = unit;
+            Calories = calories;
+            FoodGroup = foodGroup;
+        }
     }
 }
-
