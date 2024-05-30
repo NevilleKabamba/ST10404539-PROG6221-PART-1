52cf52693c308e9b1691d9ec7ec9ae4cb36644e0
NevilleKabamba <st10226298@vcconnect.edu.za>
2024-05-30 02:31:15 +0200

    Comments update

--- RecipeApp/Ingredient.cs
+++ RecipeApp/Ingredient.cs
@@ -2,11 +2,11 @@
 {
     public class Ingredient
     {
-        public string Name { get; set; }
-        public double Quantity { get; set; }
-        public string Unit { get; set; }
-        public int Calories { get; set; }
-        public string FoodGroup { get; set; }
+        public string Name { get; set; } // Name of the ingredient
+        public double Quantity { get; set; } // Quantity of the ingredient
+        public string Unit { get; set; } // Unit of measurement for the ingredient quantity
+        public int Calories { get; set; } // Calories in the ingredient
+        public string FoodGroup { get; set; } // Food group of the ingredient
 
         public Ingredient(string name, double quantity, string unit, int calories, string foodGroup)
         {
