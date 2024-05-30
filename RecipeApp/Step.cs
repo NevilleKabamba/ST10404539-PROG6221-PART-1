81921186ce52ed9b8e8d219ed49045b08d30b11e
NevilleKabamba <st10226298@vcconnect.edu.za>
2024-05-30 02:19:34 +0200

    Steps Update

--- RecipeApp/Step.cs
+++ RecipeApp/Step.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RecipeApp
+﻿namespace RecipeApp
 {
     public class Step
     {
-        public string Description { get; set; } // Description of the step
+        public string Description { get; set; }
+
+        public Step(string description)
+        {
+            Description = description;
+        }
     }
 }
-
