e56a5f2c11d9766fb69ed67baa22a2f1ce153e7e
NevilleKabamba <st10404539@vcconnect.edu.za>
2024-06-26 06:25:23 +0200

    Comments

--- RecipeAppWPF/RecipeAppWPF/Recipe.cs
+++ RecipeAppWPF/RecipeAppWPF/Recipe.cs
@@ -17,19 +17,19 @@ namespace RecipeAppWPF
             Steps = new List<Step>();
             _notifyHighCalories = notifyHighCalories;
         }
-        // Adds an ingredient to the recipe and checks for high calories
+        // Adds an ingredient to the recipe and checks for high calories.
         public void AddIngredient(Ingredient ingredient)
         {
             Ingredients.Add(ingredient);
             CheckCalories();
         }
 
-        // Adds a step to the recipe
+        // Adds a step to the recipe.
         public void AddStep(Step step)
         {
             Steps.Add(step);
         }
-        // Scales the recipe by a factor 
+        // Scales the recipe by a factor.
         public void ScaleRecipe(double factor)
         {
             foreach (var ingredient in Ingredients)
@@ -38,7 +38,7 @@ namespace RecipeAppWPF
             }
         }
 
-        // Resets the quantities of the ingredients 
+        // Resets the quantities of the ingredients.
         public void ResetQuantities()
         {
             foreach (var ingredient in Ingredients)
@@ -47,7 +47,7 @@ namespace RecipeAppWPF
             }
         }
 
-        // Calculates the total calories of the recipe 
+        // Calculates the total calories of the recipe.
         public double TotalCalories()
         {
             double totalCalories = 0;
@@ -58,7 +58,7 @@ namespace RecipeAppWPF
             return totalCalories;
         }
 
-        // Checks if the total calories exceed 300 and notifies if they do
+        // Checks if the total calories exceed 300 and notifies if they do.
         private void CheckCalories()
         {
             if (TotalCalories() > 300)
