4564a021fac2e417e7b3a8e91b65fda69ae4bd3a
NevilleKabamba <st10404539@vcconnect.edu.za>
2024-06-26 06:26:54 +0200

    log

--- RecipeAppWPF/RecipeAppWPF/Step.cs
+++ RecipeAppWPF/RecipeAppWPF/Step.cs
@@ -3,7 +3,7 @@
     public class Step
     {
         public string Description { get; set; }
-        // Constructor to initialize a Step object
+        // Constructor to initialize a Step object.
         public Step(string description)
         {
             Description = description;
